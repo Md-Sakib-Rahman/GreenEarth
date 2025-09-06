6b4ec8fa6ff2aed0f31ff2edcc772e4cfc7d77ee
Md-Sakib-Rahman <101838652+Md-Sakib-Rahman@users.noreply.github.com>
2025-09-07 03:01:54 +0600

    added navbar and hero section with responsiveness

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+    {
+      "tailwindCSS.emmetCompletions": true,
+      "editor.inlineSuggest.enabled": true,
+      "editor.quickSuggestions": {
+        "strings": true
+      },
+      "css.validate": false, // Disable default CSS validation to prevent conflicts
+      "tailwindCSS.includeLanguages": {
+        "html": "html",
+        "javascript": "javascript" // Include other languages if you use Tailwind classes there
+      }
+    }
